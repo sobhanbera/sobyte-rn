044b53e6c1911ee6356c78e48eb5c6c3f6c69bc9
Sobhan Bera <sobhanbera258@gmail.com>
2022-06-22 14:44:37 +0530

    test will work fine now

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,20 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import App from '../App'
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
+
+const response: any = {} // just a bare variable to reduce error in resolve statement below...
+global.fetch = jest.fn(
+    () => new Promise((resolve, _reject) => resolve(response)),
+)
+jest.mock('react-native-gesture-handler', () => {})
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
