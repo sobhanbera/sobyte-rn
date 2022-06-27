400303683ed749d6ab329c728788d0cf096e882a
sobhanbera <sobhanbera258@gmail.com>
2022-06-27 14:18:23 +0530

    new constants added
    with the introduction of regex type of constants

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -8,6 +8,52 @@
  * Purpose - any type of constants...
  */
 
+import {Dimensions} from 'react-native'
+
+// dimension instance for the device
+export const WINDOW_DIMENSION = Dimensions.get('window')
+export const SCREEN_DIMENSION = Dimensions.get('screen')
+// the window's dimenstions and diff parts
+export const WINDOW_WIDTH = WINDOW_DIMENSION.width // window's width
+export const WINDOW_HEIGHT = WINDOW_DIMENSION.height // window's height
+export const WINDOW_SCALE = WINDOW_DIMENSION.scale // window's scale
+export const WINDOW_FONT_SCALE = WINDOW_DIMENSION.fontScale // window's font scale
+// the screen's dimenstions and diff parts
+export const SCREEN_WIDTH = SCREEN_DIMENSION.width // screen's width
+export const SCREEN_HEIGHT = SCREEN_DIMENSION.height // screen's height
+export const SCREEN_SCALE = SCREEN_DIMENSION.scale // screen's scale
+export const SCREEN_FONT_SCALE = SCREEN_DIMENSION.fontScale // screen's font scale
+
+export const MAX_DISPLAY_TEXT_LENGTH = 30 // length of the text after which "..." will be shown
+
+// IMAGE CONSTANTS ABOUT QUALITIES.
+export const DEFAULT_ARTWORK_SIZE = 200 // default image width and height in string...
+export const DEFAULT_ARTWORK_QUALITY = 90 // default image quality in string...
+
+export const DEFAULT_LARGE_ARTWORK_SIZE = 576 // default high quality image width and height in string...
+export const DEFAULT_LARGE_ARTWORK_QUALITY = 100 // default high quality image quality in string...
+
+export const DEFAULT_EXTRA_LARGE_ARTWORK_SIZE = 720 // default high quality image width and height in string...
+export const DEFAULT_EXTRA_LARGE_ARTWORK_QUALITY = 100 // default high quality image quality in string...
+
+export const DEFAULT_PLAYER_ARTWORK_SIZE = 400 // default size image for the music player UI in string...
+export const DEFAULT_PLAYER_ARTWORK_QUALITY = 100 // default quality image quality for music player UI string...
+
+// below value should not be changed regarding the shades on notification panel
+export const DEFAULT_NOTIFICATION_ARTWORK_SIZE = 250 // the size (width/height) of the image which will be shown in the notification of when a track is played short size of image would take less time to load and play the song faster then previously...
+export const DEFAULT_NOTIFICATION_ARTWORK_QUALITY = 90 // the quality of the image which will be shown in the notification of when a track is played short size of image would take less time to load and play the song faster then previously...
+
+export const MUSIC_PLAYER_BLUR = 25 // blur value of music player image background
+
+/**
+ * music player interface constants and vars...
+ */
+export const TRACK_DATA_OVERFLOW_HEIGHT = 70
+export const TRACK_ARTWORK_SPACING = 10
+export const TRACK_ARTWORK_WIDTH = SCREEN_WIDTH * 0.82
+export const TRACK_ARTWORK_HEIGHT = TRACK_ARTWORK_WIDTH
+export const NUMBER_OF_VISIBLE_PLAYER_TRACKS = 2
+
 // user agent while making api request
 export const MUSIC_API_USER_AGENT =
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
---
+++ src/configs/regex.ts
@@ -0,0 +1,19 @@
+/**
+ * © Sobyte
+ *
+ * @author : Sobhan Bera (sobhanbera)
+ * @other_editors :
+ * @file : Typescript
+ *
+ * Purpose - every type of regex...
+ */
+
+// regex for the part of link artwork url in every songObject
+export const ARTWORK_HEIGHT_WIDTH_PART =
+    /w[0-9]{2,4}-h[0-9]{2,4}-l[0-9]{2,3}-rj/
+// a second type of artwork, this contains a 's' between height and quality
+export const ARTWORK_HEIGHT_WIDTH_PART_WITH_SIZE =
+    /w[0-9]{2,4}-h[0-9]{2,4}-s-l[0-9]{2,3}-rj/
+
+// enclosures (brackets, parans, braces) surrounded text
+export const ENCLOSURES_SURROUNDED_TEXT = / *\([^)]*\) */g
