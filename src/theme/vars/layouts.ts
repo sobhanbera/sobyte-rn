2c6ba2d7fd5889a0255adb38ec533bead40b124f
sobhanbera <sobhanbera258@gmail.com>
2022-07-11 12:48:54 +0530

    new layout style

--- src/theme/vars/layouts.ts
+++ src/theme/vars/layouts.ts
@@ -77,6 +77,9 @@ export default function GetLayouts() {
         justifyContentBetween: {
             justifyContent: 'space-between',
         },
+        justifyContentEvenly: {
+            justifyContent: 'space-evenly',
+        },
         scrollSpaceAround: {
             flexGrow: 1,
             justifyContent: 'space-around',
