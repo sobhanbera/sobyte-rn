23f5964e9344f23d1b3d3daf38c1b6888483417d
sobhanbera <sobhanbera258@gmail.com>
2022-07-16 20:31:06 +0530

    new constant link

--- src/configs/endpoints.ts
+++ src/configs/endpoints.ts
@@ -19,3 +19,10 @@ export const PRIMARY_MUSIC_API_ENDPOINTS = {
     player: 'player',
     get_search_suggestions: 'music/get_search_suggestions',
 }
+
+// the image of the logo along with name of app
+export const APP_LOGO_WITH_NAME_IMG_LINK =
+    'https://raw.githubusercontent.com/sobhanbera/sobyte/main/.github_src/named_logo.png?token=GHSAT0AAAAAABQFG2BGPQJ52HNZZLJ44FAKYWS34QA'
+
+// this is a random route URL which doesn't exists currently
+export const NOTHING_URL = 'https://sobhanbera.com/sobyte/image'
